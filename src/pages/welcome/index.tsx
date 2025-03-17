6fb190f84082f455b88f7c583c124894bca8b0b3
Lauro Floresta <lauro.floresta@gmail.com>
2025-03-17 15:22:43 -0300

    chore: clean up Vite boilerplate and add CSS reset

---
+++ src/pages/welcome/index.tsx
@@ -0,0 +1,11 @@
+import { Container, LogoContainer, PresentationContainer } from "./styled"
+
+export const Welcome = () => {
+  return (
+    <Container>
+      <LogoContainer />
+      <PresentationContainer>
+      </PresentationContainer>
+    </Container>
+  )
+}
---
+++ src/pages/welcome/styled.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #e7aa4a;
+  width: 100vw;
+  height: 100vh;
+`
+
+export const LogoContainer = styled.div``
+
+export const Message = styled.p``
+
+
+export const LoginButton = styled.button``
+
+
+export const PresentationContainer = styled.div``
